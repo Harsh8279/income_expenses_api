b6633975a4e33aa72559eab98d3429f85a65889f
Harsh8279 <hhenu8279@gmail.com>
2021-08-25 23:28:44 +0530

    User app and Register User Api Done

--- incomeexpensesapi/settings.py
+++ incomeexpensesapi/settings.py
@@ -27,6 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+AUTH_USER_MODEL = 'authentication.User'
 
 # Application definition
 
@@ -37,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'authentication'
 ]
 
 MIDDLEWARE = [
@@ -80,6 +83,10 @@ DATABASES = {
     }
 }
 
+
+REST_FRAMEWORK = {
+    'NON_FIELD_ERRORS_KEY':'error'
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
