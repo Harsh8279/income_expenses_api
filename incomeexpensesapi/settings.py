f66b8e0b8ea7bd859e77fccbb965991a2456b163
Harsh8279 <hhenu8279@gmail.com>
2021-08-26 01:15:12 +0530

    settings for mail

--- incomeexpensesapi/settings.py
+++ incomeexpensesapi/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -85,7 +85,10 @@ DATABASES = {
 
 
 REST_FRAMEWORK = {
-    'NON_FIELD_ERRORS_KEY':'error'
+    'NON_FIELD_ERRORS_KEY':'error',
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
 }
 
 # Password validation
@@ -130,3 +133,10 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'email.sender852@gmail.com'
+EMAIL_HOST_PASSWORD = ''
+print(f"\n\t===> {EMAIL_HOST_USER}")
